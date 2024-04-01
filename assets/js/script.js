a88da331325a133ba7a5d8883c635f9d5014effb
EstebanCortez <estebanleocortez27@gmail.com>
2024-04-01 19:28:54 -0400

    Agregando JS al proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $("#boton-enviar").click(function(){
+
+        alert("El Mensaje fue enviado");
+
+    })
+
+    $("#quienes-somos").click(function(){
+
+        $("#quienes-somos").hide();
+
+    })
+
+    $("#destacados").click(function(){
+
+        $("#destacados").hide();
+
+    })
+
+
+})
+
+
+
